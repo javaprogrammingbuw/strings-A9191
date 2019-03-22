568a14754e262c58c15143270a5a75084cc61903
afshin.Ghasemi@uni-weimar.de <yepu7258@in.uni-weimar.de>
2019-03-22 17:21:21 +0100

    <complete>

--- strings/Expand.java
+++ strings/Expand.java
@@ -1,41 +1,12 @@
-package strings;
-
 import java.util.Scanner;
 
-public class Expand {  
-	static String input;
-	static char c;
+//advanced
+
+public class Expand {
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter String");
-		input = scan.nextLine();
-		char a = Expandstring(input);
-		System.out.println(a);
-	}	
-	
-	public static char Expandstring(String a){
-	String pot="0";
-	for(int i=0;i<input.length();i++){
-		c=input.charAt(i);
-		if(Character.isDigit(c)){
-			pot=pot+c;
-        }
-		else{
-        	int n=Integer.parseInt(pot);
-        	if(n==0){
-        		System.out.print(c);
-        		}
-        	else{
-        		for(int j=0;j<n;j++){
-        		System.out.print(c);
-        	    }
-        	}       	
-        	pot="0";
-        }
-	}
-	return c;
-	} 
-}
+		
 		//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
 		//expand the String by printing each letter so many times as the number before the letter indicates
 		//e.g. 2a3b5c -> aabbbccccc
@@ -44,4 +15,6 @@ public class Expand {
 		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
 		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee  
 		
-
+	}
+	
+}
---
+++ strings/Palindrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Palindrome{
+	
+
+	
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		input = scan.nextint();
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	}
+}
+	//todo: Create a method isPalindrome which takes a String as input and returns true if it is a palindrome and false otherwise.
+	//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
+	//Ignore upper and lower case. 'Hannah' is a palindrome, too.
+	//Ignore also spaces. 'Taco cat' is a valid palindrome.
+
